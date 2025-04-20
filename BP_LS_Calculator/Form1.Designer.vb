b6d5828c18088d1917679a6ea5eff651508caa6b
viickel <v.thivolle@gmail.com>
2025-04-20 18:52:47 +0200

    Ajoutez des fichiers projet.

---
+++ BP_LS_Calculator/Form1.Designer.vb
@@ -0,0 +1,166 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        DataGridView1 = New DataGridView()
+        Nom = New DataGridViewTextBoxColumn()
+        Prenom = New DataGridViewTextBoxColumn()
+        Licence = New DataGridViewTextBoxColumn()
+        Cotation = New DataGridViewTextBoxColumn()
+        NbVictoire = New DataGridViewTextBoxColumn()
+        SommePoints = New DataGridViewTextBoxColumn()
+        TieBreaker = New DataGridViewTextBoxColumn()
+        SplitContainer1 = New SplitContainer()
+        PictureBox1 = New PictureBox()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(SplitContainer1, ComponentModel.ISupportInitialize).BeginInit()
+        SplitContainer1.Panel1.SuspendLayout()
+        SplitContainer1.Panel2.SuspendLayout()
+        SplitContainer1.SuspendLayout()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(12, 12)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(113, 46)
+        Button1.TabIndex = 0
+        Button1.Text = "Ouvrir fichier Cotcot"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.AllowUserToOrderColumns = True
+        DataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Columns.AddRange(New DataGridViewColumn() {Nom, Prenom, Licence, Cotation, NbVictoire, SommePoints, TieBreaker})
+        DataGridView1.Dock = DockStyle.Fill
+        DataGridView1.Location = New Point(0, 0)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.Size = New Size(649, 365)
+        DataGridView1.TabIndex = 4
+        ' 
+        ' Nom
+        ' 
+        Nom.DataPropertyName = "Nom"
+        Nom.HeaderText = "Nom"
+        Nom.Name = "Nom"
+        ' 
+        ' Prenom
+        ' 
+        Prenom.DataPropertyName = "Prenom"
+        Prenom.HeaderText = "Prénom"
+        Prenom.Name = "Prenom"
+        ' 
+        ' Licence
+        ' 
+        Licence.DataPropertyName = "NumLicence"
+        Licence.HeaderText = "Licence"
+        Licence.Name = "Licence"
+        ' 
+        ' Cotation
+        ' 
+        Cotation.DataPropertyName = "Stats.PointsCotation"
+        Cotation.HeaderText = "Cotation"
+        Cotation.Name = "Cotation"
+        ' 
+        ' NbVictoire
+        ' 
+        NbVictoire.DataPropertyName = "Stats.NbVictoires"
+        NbVictoire.HeaderText = "NbVictoire"
+        NbVictoire.Name = "NbVictoire"
+        ' 
+        ' SommePoints
+        ' 
+        SommePoints.DataPropertyName = "Stats.SommePoints"
+        SommePoints.HeaderText = "SommePoints"
+        SommePoints.Name = "SommePoints"
+        ' 
+        ' TieBreaker
+        ' 
+        TieBreaker.DataPropertyName = "Stats.TieBreaker"
+        TieBreaker.HeaderText = "TieBreaker"
+        TieBreaker.Name = "TieBreaker"
+        ' 
+        ' SplitContainer1
+        ' 
+        SplitContainer1.Dock = DockStyle.Fill
+        SplitContainer1.Location = New Point(0, 0)
+        SplitContainer1.Name = "SplitContainer1"
+        SplitContainer1.Orientation = Orientation.Horizontal
+        ' 
+        ' SplitContainer1.Panel1
+        ' 
+        SplitContainer1.Panel1.Controls.Add(PictureBox1)
+        SplitContainer1.Panel1.Controls.Add(Button1)
+        ' 
+        ' SplitContainer1.Panel2
+        ' 
+        SplitContainer1.Panel2.Controls.Add(DataGridView1)
+        SplitContainer1.Size = New Size(649, 450)
+        SplitContainer1.SplitterDistance = 81
+        SplitContainer1.TabIndex = 5
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Image = My.Resources.Resources.ASL_FFE___Logo_noir_BANNIERE
+        PictureBox1.ImageLocation = ""
+        PictureBox1.Location = New Point(400, 6)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(243, 70)
+        PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+        PictureBox1.TabIndex = 1
+        PictureBox1.TabStop = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(649, 450)
+        Controls.Add(SplitContainer1)
+        Name = "Form1"
+        Text = "Form1"
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        SplitContainer1.Panel1.ResumeLayout(False)
+        SplitContainer1.Panel2.ResumeLayout(False)
+        CType(SplitContainer1, ComponentModel.ISupportInitialize).EndInit()
+        SplitContainer1.ResumeLayout(False)
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Nom As DataGridViewTextBoxColumn
+    Friend WithEvents Prenom As DataGridViewTextBoxColumn
+    Friend WithEvents Licence As DataGridViewTextBoxColumn
+    Friend WithEvents Cotation As DataGridViewTextBoxColumn
+    Friend WithEvents NbVictoire As DataGridViewTextBoxColumn
+    Friend WithEvents SommePoints As DataGridViewTextBoxColumn
+    Friend WithEvents TieBreaker As DataGridViewTextBoxColumn
+    Friend WithEvents SplitContainer1 As SplitContainer
+    Friend WithEvents PictureBox1 As PictureBox
+
+End Class
---
+++ BP_LS_Calculator/Form1.vb
@@ -0,0 +1,225 @@
+﻿Imports System.Xml
+
+
+Public Class Form1
+
+    Public Class CombattantStats
+        Public PointsCotation As Integer = 0
+        Public NbVictoires As Integer = 0
+        Public SommePoints As Integer = 0
+        Public TieBreaker As Long = 0
+
+
+        ' Méthode pour ajouter des points de cotation et mettre à jour les statistiques
+        Public Sub AjouterPoints(scoreGagnant As Integer, scorePerdant As Integer)
+
+            NbVictoires += 1
+            SommePoints += scoreGagnant
+
+            Dim difference As Integer = Math.Abs(scoreGagnant - scorePerdant)
+            If difference = 1 Then
+                PointsCotation += 1
+                TieBreaker += 1
+            ElseIf difference >= 4 AndAlso difference <= 7 Then
+                PointsCotation += 2
+                TieBreaker += 100
+            ElseIf difference >= 8 AndAlso difference <= 11 Then
+                PointsCotation += 3
+                TieBreaker += 10000
+            ElseIf difference >= 12 Then
+                PointsCotation += 4
+                TieBreaker += 1000000
+            End If
+        End Sub
+    End Class
+
+    Public Class CombatantInfo
+        Public ID As String
+        Public Nom As String
+        Public Prenom As String
+        Public NumLicence As String
+        Public Stats As New CombattantStats()
+    End Class
+
+
+
+    Public ListeCombatants As New List(Of CombatantInfo)
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim OpenFileDialog As New OpenFileDialog()
+        OpenFileDialog.Filter = "Fichiers BellePoule (*.cotcot)|*.cotcot|Tous les fichiers (*.*)|*.*"
+        OpenFileDialog.Title = "Sélectionnez un fichier .cotcot"
+
+        If OpenFileDialog.ShowDialog() = DialogResult.OK Then
+            Dim sCheminFichier As String = OpenFileDialog.FileName
+            ListeCombatants.Clear()
+            CotcotReader(sCheminFichier)
+
+
+        End If
+
+    End Sub
+
+
+
+    Private Sub CotcotReader(sFilePath As String)
+        Try
+            Dim settings As New XmlReaderSettings()
+            settings.DtdProcessing = DtdProcessing.Parse
+
+            Using reader As XmlReader = XmlReader.Create(sFilePath, settings)
+                Dim iNbTour As Integer = 0
+
+                ' Se déplacer jusqu'à l'élément <Tireurs>
+                If reader.ReadToFollowing("Tireurs") Then
+                    ' Lire les éléments <Tireur> à l'intérieur de <Tireurs>
+                    While reader.Read()
+                        If reader.NodeType = XmlNodeType.Element AndAlso reader.Name = "Tireur" Then
+                            Dim nouveauCombattant As New CombatantInfo() With {
+                            .ID = reader.GetAttribute("ID"),
+                            .Nom = reader.GetAttribute("Nom"),
+                            .Prenom = reader.GetAttribute("Prenom"),
+                            .NumLicence = reader.GetAttribute("Licence")
+                        }
+                            ListeCombatants.Add(nouveauCombattant)
+                        ElseIf reader.NodeType = XmlNodeType.EndElement AndAlso reader.Name = "Tireurs" Then
+                            ' Sortir de la boucle une fois que l'élément </Tireurs> est atteint
+                            Exit While
+                        End If
+                    End While
+                End If
+
+                '-----------------------------------------------------------------------------------------------------
+
+                ' Se déplacer jusqu'à l'élément <TourDePoules>
+                If reader.ReadToFollowing("TourDePoules") Then
+                    ' Parcourir les éléments <Poule>
+                    While reader.ReadToFollowing("Poule")
+                        ' Lire les matchs à l'intérieur de chaque <Poule>
+                        Using pouleReader As XmlReader = reader.ReadSubtree()
+                            pouleReader.Read() ' Avancer au premier élément à l'intérieur de <Poule>
+                            While pouleReader.Read()
+                                If pouleReader.NodeType = XmlNodeType.Element AndAlso pouleReader.Name = "Match" Then
+                                    Dim idMatch As String = pouleReader.GetAttribute("ID")
+                                    Console.WriteLine($"Lecture du match ID: {idMatch}")
+
+                                    Dim tireur1Ref As String = ""
+                                    Dim tireur1Score As Integer = -1
+                                    Dim tireur2Ref As String = ""
+                                    Dim tireur2Score As Integer = -1
+
+                                    ' Lire les informations des tireurs dans le match
+                                    Using matchReader As XmlReader = pouleReader.ReadSubtree()
+                                        matchReader.Read()
+                                        While matchReader.ReadToFollowing("Tireur")
+                                            Dim refTireurMatch As String = matchReader.GetAttribute("REF")
+                                            Dim scoreStr As String = matchReader.GetAttribute("Score")
+                                            Dim statutTireur As String = matchReader.GetAttribute("Statut")
+                                            Dim score As Integer
+
+                                            If Not String.IsNullOrEmpty(refTireurMatch) AndAlso Integer.TryParse(scoreStr, score) Then
+                                                If tireur1Ref = "" Then
+                                                    tireur1Ref = refTireurMatch
+                                                    tireur1Score = score
+                                                Else
+                                                    tireur2Ref = refTireurMatch
+                                                    tireur2Score = score
+                                                End If
+
+                                                If statutTireur.ToUpper() = "V" Then
+                                                    Dim combattantVainqueur = ListeCombatants.FirstOrDefault(Function(c) c.ID = refTireurMatch)
+                                                    If combattantVainqueur IsNot Nothing Then
+                                                        Dim refPerdant As String = If(refTireurMatch = tireur1Ref, tireur2Ref, tireur1Ref)
+                                                        Dim scorePerdant As Integer = If(refTireurMatch = tireur1Ref, tireur2Score, tireur1Score)
+                                                        combattantVainqueur.Stats.AjouterPoints(score, scorePerdant)
+                                                        Console.WriteLine($"  Le tireur {refTireurMatch} (ID) a gagné contre {refPerdant} ({scorePerdant}).")
+                                                    End If
+                                                End If
+                                            End If
+                                        End While
+                                    End Using
+                                End If
+                            End While
+                        End Using
+                    End While
+                End If
+
+
+
+                AffichageDataGrid()
+
+
+
+
+
+            End Using
+        Catch ex As Exception
+            MessageBox.Show("Erreur lors de la lecture du fichier : " & ex.Message, "Erreur")
+        End Try
+    End Sub
+
+
+
+    Public Sub AffichageDataGrid()
+
+        If Me.DataGridView1 IsNot Nothing Then
+            ' Trier la liste des combattants en utilisant une fonction de comparaison personnalisée
+            ListeCombatants.Sort(Function(c1 As CombatantInfo, c2 As CombatantInfo)
+                                     ' Comparer par PointsCotation (ordre décroissant)
+                                     Dim comparison As Integer = c2.Stats.PointsCotation.CompareTo(c1.Stats.PointsCotation)
+                                     If comparison <> 0 Then
+                                         Return comparison
+                                     End If
+
+                                     ' Si PointsCotation est égal, comparer par NbVictoires (ordre décroissant)
+                                     comparison = c2.Stats.NbVictoires.CompareTo(c1.Stats.NbVictoires)
+                                     If comparison <> 0 Then
+                                         Return comparison
+                                     End If
+
+                                     ' Si NbVictoires est égal, comparer par SommePoints (ordre décroissant)
+                                     comparison = c2.Stats.SommePoints.CompareTo(c1.Stats.SommePoints)
+                                     If comparison <> 0 Then
+                                         Return comparison
+                                     End If
+
+                                     ' Si SommePoints est égal, comparer par TieBreaker (ordre décroissant)
+                                     Return c2.Stats.TieBreaker.CompareTo(c1.Stats.TieBreaker)
+                                 End Function)
+
+
+            ' Empêcher l'édition des cellules
+            Me.DataGridView1.ReadOnly = True
+            ' Empêcher l'ajout de nouvelles lignes par l'utilisateur (via l'interface)
+            Me.DataGridView1.AllowUserToAddRows = False
+            ' Empêcher la suppression de lignes par l'utilisateur (via l'interface)
+            Me.DataGridView1.AllowUserToDeleteRows = False
+            Me.DataGridView1.Columns.Clear() ' Efface les colonnes existantes
+
+            ' Ajoute les colonnes manuellement
+            Me.DataGridView1.Columns.Add("Classement", "Classement")
+            Me.DataGridView1.Columns.Add("Nom", "Nom")
+            Me.DataGridView1.Columns.Add("Prenom", "Prénom")
+            Me.DataGridView1.Columns.Add("NumLicence", "Licence")
+            Me.DataGridView1.Columns.Add("PointsCotation", "Cotation")
+            Me.DataGridView1.Columns.Add("NbVictoires", "NbVictoires")
+            Me.DataGridView1.Columns.Add("SommePoints", "SommePoints")
+            Me.DataGridView1.Columns.Add("TieBreaker", "TieBreaker")
+
+            ' Remplis les lignes avec les données de ListeCombatants
+            For i As Integer = 0 To ListeCombatants.Count - 1
+                Dim combattant As CombatantInfo = ListeCombatants(i)
+                Me.DataGridView1.Rows.Add(i + 1, combattant.Nom, combattant.Prenom, combattant.NumLicence,
+                                           combattant.Stats.PointsCotation, combattant.Stats.NbVictoires,
+                                           combattant.Stats.SommePoints, combattant.Stats.TieBreaker)
+            Next
+
+
+        End If
+    End Sub
+
+End Class
